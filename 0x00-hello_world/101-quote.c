46f5d97b86c24a7631a0fafd6a451dea57e2704b
Dev <kevguama@gmail.com>
2023-03-16 17:49:23 -0700

    Revion Qn8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,11 @@
 #include <unistd.h>
 /**
  * main - Entry point
- * Return: Always 1 (Success)
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+return (1);
 }
