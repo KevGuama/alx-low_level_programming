2a86eb879f13d17b47bd27742d540afce0bfaf8e
Dev <kevguama@gmail.com>
2023-04-03 13:16:12 -0700

    1. memcpy1.0

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,14 @@
+#include <main.h>
+/* simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ *Return: Nothing.
+ */
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		*(dest + i) =  *(src + i);
+
+	return (dest);
+}
