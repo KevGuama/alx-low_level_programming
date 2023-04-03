b93dab64a106d1ceb76d1470e071b0bbfb245a93
Dev <kevguama@gmail.com>
2023-04-03 13:51:47 -0700

    4. strpbrk2.0

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -6,13 +6,21 @@
  * @accept: 2nd string.
  * Return: a pointer to the byte in s that matches one of the
  */
+#include "main.h"
+
+/**
+ * _strpbrk - look up a string for each set of bytes.
+ * @s: 1st string.
+ * @accept: 2nd string.
+ * Return: a pointer to the byte in s that matches one of the
+ */
 char *_strpbrk(char *s, char *accept)
 {
 	unsigned int i, j;
 
 	for (i = 0; *(s + i) != '\0'; i++)
 	{
-		for (j = 0; *(accept + j) != '\0';)
+		for (j = 0; *(accept + j) != '\0'; j++)
 		{
 			if (*(s + i) == *(accept + j))
 				return (s + i);
