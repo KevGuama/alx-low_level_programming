336992215235a5770b5d897a771620d9df18faa7
Dev <kevguama@gmail.com>
2023-04-11 01:19:56 +0300

    1. Silence is_3.0

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,12 +1,25 @@
+#include <stdlib.h>
 #include <stdio.h>
+
 /**
- * main - prints the number of arguments passed into it.
- * @argc: number of command line arguments.
- * @argv: array that contains the program command line arguments.
- * Return: 0 - success.
- */
-int main(int argc, char *argv[] __attribute__((unused)))
+ * main - a program that prints the number of arguments
+ *        passed into it
+ *
+ * @argc: holds the number of arguments passed
+ * @argv: array pointer that holds the arguments passed
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(int argc, char __attribute__((unused)) *argv[])
 {
-	printf("%d\n", argc - 1);
+	int i = 0, m;
+
+	while (i < argc)
+	{
+		m = i;
+		i++;
+	}
+	printf("%d\n", m);
 	return (0);
 }
