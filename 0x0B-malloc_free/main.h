18e2f213580dcf32c6cf31248b2d1ff90e362335
Dev <kevguama@gmail.com>
2023-04-11 15:38:18 -0700

    0. Float like_3.0

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,6 +1,7 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef MAIN_H
+#define MAIN_H
 
+int _putchar(char c); /*never used*/
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
