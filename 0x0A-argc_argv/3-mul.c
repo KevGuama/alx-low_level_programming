4429ab97853058a73b10dc8f1f06527998f170ec
Dev <kevguama@gmail.com>
2023-04-10 17:05:47 -0700

    AllinBetty8.0

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -7,25 +7,23 @@
  * @argc: holds the number of arguments passed
  * @argv: array pointer that holds the arguments passed
  *
- * Return: Always 0 (Success)
-*/
-
-int main(int argc, char **argv)
+ * Return: Always 0 
+ */
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiplies two numbers.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
+ */
+int main(int argc, char *argv[])
 {
-int x, y;
-
-
-if (argc < 3)
-{
-printf("Error\n");
-return (1);
-}
-
-
-x = atoi(argv[1]);
-y = atoi(argv[2]);
-printf("%d\n", x * y);
-
-
-return (0);
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
 }
